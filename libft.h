767b7189b424c9408c24104a8a48f73e22aef1dc
DaniMSalinas <danideringer7@gmail.com>
2022-09-20 15:00:09 +0200

    libft.h con norma

--- libft.h
+++ libft.h
@@ -6,16 +6,16 @@
 /*   By: dmaldona <dmaldona@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 11:07:00 by dmaldona          #+#    #+#             */
-/*   Updated: 2022/09/20 14:52:51 by dmaldona         ###   ########.fr       */
+/*   Updated: 2022/09/20 14:59:51 by dmaldona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 /** simple functions **/
 /* characters functions */
@@ -32,7 +32,7 @@ int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void    *ft_memset(void *b, int c, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 
@@ -45,7 +45,7 @@ char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *h, const char *n, size_t len);
-int     ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 /** complex functions **/
 char	*ft_strjoin(char const *s1, char const *s2);
